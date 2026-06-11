204c192722a41c650c6a8aa387b1c0cd7dda501b
hideldev78 <hidelberg.martinez@dcsla.com>
2026-06-11 12:37:55 -0400

    [FIX] ajuste de fichas

--- models/stock_lot.py
+++ models/stock_lot.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockLot(models.Model):
@@ -7,3 +7,50 @@ class StockLot(models.Model):
     body_serial = fields.Char(string="Serial de la carroceria", copy=False)
     engine_serial = fields.Char(string="Serial del motor", copy=False)
     vehicle_plate = fields.Char(string="Placa", copy=False)
+    vehicle_information_ids = fields.One2many(
+        "vehicle.information",
+        "lot_id",
+        string="Vehiculos vinculados",
+        readonly=True,
+    )
+    vehicle_information_id = fields.Many2one(
+        "vehicle.information",
+        string="Vehiculo",
+        compute="_compute_vehicle_trade_fields",
+    )
+    management_profile_id = fields.Many2one(
+        "vehicle.management.profile",
+        string="Perfil de gestion",
+        compute="_compute_vehicle_trade_fields",
+    )
+    vehicle_origen_id = fields.Many2one(
+        "vehicle.origen",
+        string="Origen del vehiculo",
+        compute="_compute_vehicle_trade_fields",
+    )
+    vehicle_stock_type_id = fields.Many2one(
+        "vehicle.stock.type",
+        string="Tipo de stock",
+        compute="_compute_vehicle_trade_fields",
+    )
+    vehicle_type_unit_id = fields.Many2one(
+        "vehicle.type.unit",
+        string="Tipo de unidad",
+        compute="_compute_vehicle_trade_fields",
+    )
+
+    @api.depends(
+        "vehicle_information_ids",
+        "vehicle_information_ids.management_profile_id",
+        "vehicle_information_ids.vehicle_origen_id",
+        "vehicle_information_ids.vehicle_stock_type_id",
+        "vehicle_information_ids.vehicle_type_unit_id",
+    )
+    def _compute_vehicle_trade_fields(self):
+        for lot in self:
+            vehicle = lot.vehicle_information_ids[:1]
+            lot.vehicle_information_id = vehicle
+            lot.management_profile_id = vehicle.management_profile_id
+            lot.vehicle_origen_id = vehicle.vehicle_origen_id
+            lot.vehicle_stock_type_id = vehicle.vehicle_stock_type_id
+            lot.vehicle_type_unit_id = vehicle.vehicle_type_unit_id
--- models/stock_picking.py
+++ models/stock_picking.py
@@ -19,6 +19,7 @@ class StockPicking(models.Model):
                     continue
 
                 values = None
+                lot_values = {}
                 if picking.picking_type_code == "incoming":
                     values = {"lot_id": line.lot_id.id}
                 elif picking.picking_type_code == "outgoing":
@@ -31,6 +32,21 @@ class StockPicking(models.Model):
                     values["vin_number"] = line.lot_id.name
                 if line.lot_id.vehicle_plate and not vehicle.license_plate:
                     values["license_plate"] = line.lot_id.vehicle_plate
+                elif vehicle.license_plate and not line.lot_id.vehicle_plate:
+                    lot_values["vehicle_plate"] = vehicle.license_plate
+
+                if line.lot_id.body_serial and not vehicle.chassis_number:
+                    values["chassis_number"] = line.lot_id.body_serial
+                elif vehicle.chassis_number and not line.lot_id.body_serial:
+                    lot_values["body_serial"] = vehicle.chassis_number
+
+                if line.lot_id.engine_serial and not vehicle.motor_number:
+                    values["motor_number"] = line.lot_id.engine_serial
+                elif vehicle.motor_number and not line.lot_id.engine_serial:
+                    lot_values["engine_serial"] = vehicle.motor_number
+
+                if lot_values:
+                    line.lot_id.write(lot_values)
 
                 vehicle.write(values)
         return res
